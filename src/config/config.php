ce3b1fb9c7bdbddc8f897c08897c02224f360965
dubuqingfeng <1135326346@qq.com>
2018-03-14 20:58:37 +0800

    add SSL

--- src/config/config.php
+++ src/config/config.php
@@ -3,5 +3,6 @@
 return [
     'host' => env('WEBSOCKET_HOST', '0.0.0.0'),
     'port' => env('WEBSOCKET_PORT', 9501),
+    'ssl' => env('WEBSOCKET_SSL', false),
     'task_worker_num' => env('WEBSOCKET_TASK_WORKER_NUM', 8),
 ];
